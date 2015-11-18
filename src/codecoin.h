d75352a5677029119fb5c8c2159305402141d90f
The Grantcoin Foundation <mail@grantcoin.org>
2015-11-18 16:12:27 -0500

    remove references to other coins

--- src/codecoin.h
+++ src/codecoin.h
@@ -15,34 +15,16 @@ extern const CBigNum bnProofOfStakeLimit;
 //extern const unsigned int nStakeMinAge;
 //extern const unsigned int nStakeMaxAge;
 
-#ifdef BRAND_solarcoin
-#include "solarcoin.h"
-#elif BRAND_catcoin
-#include "catcoin.h"
-#elif BRAND_kittycoin
-#include "kittycoin.h"
-#elif BRAND_uro
-#include "uro.h"
-#elif BRAND_grantcoin
+#ifdef BRAND_grantcoin
 #include "grantcoin.h"
 #elif BRAND_grantstake
 #include "grantstake.h"
-#elif defined(BRAND_givecoin) || defined(BRAND_givestake)
-#include "givecoin.h"
-#elif defined(BRAND_hamburger)
-#include "hamburger.h"
 #else
 #error "No BRAND_*COIN defined!!"
 #endif
 
 #ifndef BRAND_SYM
 #define BRAND_SYM BRAND_CODE
-#endif
-
-/* main.h do we always CheckProofOfWork in CheckIndex */
-/* Litecoin does not do so for performance reasons */
-#if defined(BRAND_bluecoin) || defined(BRAND_uro) || defined(BRAND_givestake)
-#define CHECKINDEX_SLOW
 #endif
 
 #endif
