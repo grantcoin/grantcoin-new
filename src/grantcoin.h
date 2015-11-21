de520e57d4e0c4b07b054c0c8ee5d57e374f499a
The Grantcoin Foundation <mail@grantcoin.org>
2015-11-20 21:14:16 -0500

    fix MIN_TX_FEE for display in GRT not grantoshis

--- src/grantcoin.h
+++ src/grantcoin.h
@@ -17,7 +17,7 @@ static const int COIN_DECIMALS = 6; /* decimal places for coin */
 static const int64_t MAX_MONEY = 50000000000 * COIN;
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
-static const int64_t MIN_TX_FEE = CENT;
+static const int64_t MIN_TX_FEE = 0.01;
 static const int64_t MIN_TXOUT_AMOUNT = MIN_TX_FEE;
 
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
